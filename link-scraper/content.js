a9c558d4f8affa6245a96a387ed33feeeded2a72
TimFinnigan <tim.finnigan1@gmail.com>
2021-02-20 16:31:11 -0800

    add 'matches' to manifest, clean up content.js

--- link-scraper/content.js
+++ link-scraper/content.js
@@ -4,6 +4,7 @@ let links = document.links;
 
 for (let i = 0; i < links.length; i++) {
 	let url = links[i].href;
+
 	// Get Reddit links
 	if (url.includes('comments')) {
 		linkArray.push(links[i].href);
@@ -13,9 +14,7 @@ for (let i = 0; i < links.length; i++) {
 // Remove duplicates
 let uniqueUrls = [...new Set(linkArray)];
 
-console.log(uniqueUrls);
-
-// Create text document — only saves 1st link in text doc
+// Create and save text document
 let textDoc = document.createElement('a');
 
 textDoc.href = 'data:attachment/text,' + encodeURI(uniqueUrls.join('\n'));
