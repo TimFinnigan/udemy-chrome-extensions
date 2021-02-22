d554cb76cd2ab8ee59d4a205b1f83d2899c322bc
TimFinnigan <tim.finnigan1@gmail.com>
2021-02-22 14:52:50 -0800

    simplify javascript

--- minimalist-stack-overflow-theme/script.js
+++ minimalist-stack-overflow-theme/script.js
@@ -5,11 +5,7 @@ let votes = document.getElementsByClassName('js-vote-count');
 for (let i = 1; i < votes.length; i++) {
 	let element = votes[i];
 	let voteCount = parseInt(element.getAttribute('data-value'));
-	if (voteCount < 1) {
-		let num = i + 2;
-		let result = document.querySelector('.answer:nth-of-type(' + num + ')');
-		if (result) {
-			result.style.display = 'none';
-		}
+	if (voteCount < 2) {
+		element.parentNode.parentNode.parentNode.style.display = 'none';
 	}
 }
